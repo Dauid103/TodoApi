f4e79852fac3d507187017f78dee5163ee00500f
David Andersson <daviande@kth.se>
2018-11-26 11:37:02 +0100

    Created project

---
+++ TodoApi/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if(_context.ToDoItems.Count() == 0)
+            {
+                //Create a new TodoItem if none exists
+                _context.ToDoItems.Add(new TodoItem { Name = "Item1" });
+                _context.ToDoItems.Add(new TodoItem { Name = "Item2" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+        {
+            return _context.ToDoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public ActionResult<TodoItem> GetById(int Id)
+        {
+           var todoItem =  _context.ToDoItems.Where(t => t.Id == Id).FirstOrDefault();
+
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        [HttpPost]
+        public IActionResult Create(TodoItem item)
+        {
+            _context.ToDoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { Id = item.Id }, item);
+        }
+    }
+}
---
+++ TodoApi/TodoApi/Data/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> ToDoItems { get; set; }
+    }
+}
